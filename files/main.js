ba30f0e90d2b950fa9eb8c1247e3b4f1cfb37142
misterGula <mister.gula@bk.ru>
2018-12-23 15:03:01 +0300

    Add files via upload

---
+++ files/main.js
@@ -0,0 +1,14 @@
+﻿$(function() {
+    $("#my_list").collapsorz({
+        minimum: 4
+        , showText: "Развернуть >>>"
+        , hideText: "Свернуть <<<"
+    });
+    
+    $("#my_div").collapsorz({
+        minimum: 3
+        , showText: "Другие ссылки >>>"
+        , hideText: "Скрыть <<<"
+        , toggle: "a"
+    });
+});
---
+++ files/main0000.js
@@ -0,0 +1,38 @@
+﻿$(function() {
+    $("#my_list1").collapsorz({
+        minimum: 1
+        , showText: "узнать больше >>>"
+        , hideText: "свернуть <<<"
+    });
+    
+    $("#my_list2").collapsorz({
+        minimum: 1
+        , showText: "узнать больше >>>"
+        , hideText: "свернуть <<<"
+    });
+
+    $("#my_list3").collapsorz({
+        minimum: 1
+        , showText: "узнать больше >>>"
+        , hideText: "свернуть <<<"
+    });
+
+    $("#my_list4").collapsorz({
+        minimum: 1
+        , showText: "узнать больше >>>"
+        , hideText: "свернуть <<<"
+    });
+
+    $("#my_list5").collapsorz({
+        minimum: 1
+        , showText: "узнать больше >>>"
+        , hideText: "свернуть <<<"
+    });
+
+    $("#my_div").collapsorz({
+        minimum: 3
+        , showText: "Другие ссылки >>>"
+        , hideText: "Скрыть <<<"
+        , toggle: "a"
+    });
+});
